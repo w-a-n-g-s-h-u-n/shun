41470e400a5b136830047c936d34d7138da01ae5
w-a-n-g-s-h-u-n <wangshun@wangshun.org>
2020-05-20 17:22:51 +0800

    优化PageResult

--- shun-starter-parent/shun-starter-db/src/main/java/org/wangshun/shun/db/base/PageResult.java
+++ shun-starter-parent/shun-starter-db/src/main/java/org/wangshun/shun/db/base/PageResult.java
@@ -1,23 +1,50 @@
 package org.wangshun.shun.db.base;
 
 import cn.hutool.core.collection.CollUtil;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class PageResult<T> {
-    private long current = 1;
-    private long size = 10;
-    private long total = 0;
+    private Long current;
+    private Long size;
+    private Long total;
     private List<T> records = CollUtil.newLinkedList();
+    @JsonIgnore
+    private final JSONObject json = new JSONObject();
 
     public static <T> PageResult<T> empty(PageCondition<T> condition) {
-        return new PageResult<T>().setCurrent(condition.getCurrent()).setSize(condition.getSize());
+        return new PageResult<T>().setCurrent(condition.getCurrent()).setSize(condition.getSize()).setTotal(0L);
     }
 
     public static <T> PageResult<T> empty(Page<T> page) {
         return new PageResult<T>().setCurrent(page.getCurrent()).setSize(page.getSize()).setTotal(page.getTotal()).setRecords(page.getRecords());
+    }
+
+    public PageResult<T> put(String key, Object value) {
+        json.put(key, value);
+        return this;
+    }
+
+    public JSONObject toJson() {
+        if (current != null) {
+            json.put("current", current);
+        }
+        if (size != null) {
+            json.put("size", size);
+        }
+        if (total != null) {
+            json.put("total", total);
+        }
+        if (records != null) {
+            json.put("records", records);
+        }
+        return json;
     }
 }
